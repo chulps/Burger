cba8f8ae1ecc6459706d7c31733f12c7807a6d93
Chulps <chuckoward@gmail.com>
2018-03-27 20:44:18 -0700

    set up db

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,6 +3,6 @@ USE burgers_db;
 
 -- insert into table
 INSERT INTO burgers  (burger_name, devoured, dt) 
-VALUES ('Whataburger Jr', false, CURRENT_TIMESTAMP)
-, ('Patty Melt', false, CURRENT_TIMESTAMP)
-, ('Avacado Burger', false, CURRENT_TIMESTAMP);
+VALUES ('BOOSH Burger', false, CURRENT_TIMESTAMP)
+, ('Big Burger', false, CURRENT_TIMESTAMP)
+, ('Big Butt Burger', false, CURRENT_TIMESTAMP);
